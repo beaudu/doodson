2792bed44f5bebbd37384a8605f947a11fd2c4d2
François Beauducel <beauducel@gmail.com>
2024-03-21 16:05:25 +0100

    additional waves and improved display

--- README.md
+++ README.md
@@ -3,10 +3,20 @@
 ![Newton's Sun-Moon-Earth 3-body problem](newton_3bodyproblem.jpg)
 
 ## doodson.m
-This function gives the parameters for about 40 main wave components of Earth tides due to Sun and Moon (annual, semiannual, monthly, fortnightly, diurnal, semidiurnal, ...) using the harmonic development of Arthur Thomas Doodson [1890-1968]. For a given wave, it returns in a structure the 6 Doodson's arguments, wave Darwin's symbol, wave description name and the wave period in days.
+This function gives the parameters for about 60 main wave components of Earth tides due to Sun and Moon (annual, semiannual, monthly, fortnightly, diurnal, semidiurnal, ...) using the harmonic development of Arthur Thomas Doodson [1890-1968]. For a given wave, it returns in a structure the 6 Doodson's arguments, wave Darwin's symbol, wave description name and the wave period in days.
 
-### Example
+Type `doodson` without argument to display all available waves, and their associated doodson number and period.
+
+### Examples
 ```matlab
+>> doodson
+    N: 055.565, 6798.4 days
+   Sa: 056.554, 365.26 days
+  Ssa: 057.555, 182.621 days
+...
+   S4: 491.555, 0.25 day
+   M6: 655.555, 0.172508 day
+   M8: 855.555, 0.129381 day
 >> X=doodson('M2')
    X =
      symbol: {'M2'}
@@ -14,7 +24,7 @@ This function gives the parameters for about 40 main wave components of Earth ti
     doodson: [2 0 0 0 0 0]
      period: 0.5175
 ```
-Type `doodson` without argument to see the list of available waves.
+
 
 ## Author
 **François Beauducel**, [IPGP](www.ipgp.fr), [beaudu](https://github.com/beaudu), beauducel@ipgp.fr
--- doodson.m
+++ doodson.m
@@ -15,12 +15,15 @@ function X=doodson(w)
 % References:
 %    Agnew D.C. (2007), Earth Tides, in « Treatise on Geophysics: Geodesy »,
 %        T. A. Herring Ed., Elsevier, New York.
+%    Sulzbach, R., K. Balidakis, H. Dobslaw, M. Thomas (2022): TiME22: Periodic 
+%        Disturbances of the Terrestrial Gravity Potential Induced by Oceanic
+%        and Atmospheric Tides. GFZ Data Services. https://doi.org/10.5880/GFZ.1.3.2022.006
 %
 %	Author: François Beauducel <beauducel@ipgp.fr>
 %	Created: 2014-05-22
-%	Updated: 2021-01-06
+%	Updated: 2024-03-21
 
-%	Copyright (c) 2014-2021, François Beauducel, covered by BSD License.
+%	Copyright (c) 2014-2024, François Beauducel, covered by BSD License.
 %	All rights reserved.
 %
 %	Redistribution and use in source and binary forms, with or without
@@ -49,40 +52,61 @@ ww = { ...
  055.565 'N'    'Lunar Saros';
  056.554 'Sa'   'Solar annual';
  057.555 'Ssa'  'Solar semiannual';
+ 058.554 'Sta'  '';
  063.655 'MSm'  '';
  065.455 'Mm'   'Lunar monthly';
+ 065.555 '3Mm'  ''
  073.555 'MSf'  'Lunisolar synodic fortnightly';
  075.555 'Mf'   'Lunisolar fortnightly';
  085.455 'Mtm'  '';
  125.755 '2Q1'  'Larger elliptic diurnal';
+ 127.555 'sig1' ''
+ 135.555 '3Q1'  ''
  135.655 'Q1'   'Larger lunar elliptic diurnal';
  137.455 'rho1' 'Larger lunar evectional diurnal';
  145.555 'O1'   'Principal lunar declinational';
+ 145.655 '3O1'  '';
+ 147.555 'tau1' '';
  155.555 'M1'   'Smaller lunar elliptic diurnal';
  155.655 'NO1'  '';
+ 157.455 'chi1' '';
  162.556 'pi1'  '';
  163.555 'P1'   'Principal solar declination';
- 164.556 'S1'   'Solar diurnal';
+ 164.555 'S1'   'Solar diurnal (oceanic)';
+ 164.556 'S1a'  'Solar diurnal (atmospheric)';
  165.555 'K1'   'Lunisolar diurnal';
  166.554 'psi1' '';
  167.555 'phi1' '';
+ 173.655 'tet1' '';
  175.455 'J1'   'Smaller lunar elliptic diurnal';
+ 175.555 '3J1'  '';
+ 183.555 'SO1'  '';
  185.555 'OO1'  'Lunar diurnal';
+ 227.655 'eps2' '';
+ 235.655 '32N2' '';
  235.755 '2N2'  'Lunar elliptical semidiurnal second-order';
  237.555 'MU2'  'Variational';
+ 245.555 '3N2'  '';
  245.655 'N2'   'Larger lunar elliptic semidiurnal';
  247.455 'nu2'  'Larger lunar evectional';
  255.555 'M2'   'Principal lunar semidiurnal';
+ 255.655 '3M2'  '';
  263.655 'lam2' 'Smaller lunar evectional';
  265.455 'L2'   'Smaller lunar elliptic semidiurnal';
- 272.555 'T2'   'Larger solar elliptic';
+ 265.555 '3L2'  '';
+ 272.556 'T2'   'Larger solar elliptic';
  273.555 'S2'   'Principal solar semidiurnal';
  274.555 'R2'   'Smaller solar elliptic';
  275.555 'K2'   'Lunisolar semidiurnal';
+ 285.455 'eta2' '';
  291.555 '2SM2' 'Shallow water semidiurnal';
  355.555 'M3'   'Lunar terdiurnal';
  345.555 '2MK3' 'Shallow water terdiurnal';
+ 345.655 '3MN3' '';
  365.555 'MK3'  'Shallow water terdiurnal';
+ 381.555 'T3'   '';
+ 382.555 'S3'   'Solar terdiurnal';
+ 383.555 'R3'   '';
  445.655 'MN4'  'Shallow water quarter diurnal';
  455.555 'M4'   'Shallow water overtides of principal lunar';
  473.555 'MS4'  'Shallow water quarter diurnal';
@@ -134,6 +158,6 @@ if nargout > 0
 	X.period = p;
 else
 	for n = 1:length(k)
-		fprintf('%5s: %g day%s\n',ww{k(n),2},p(n),repmat('s',p(n)>1));
+		fprintf('%5s: %07.3f, %g day%s\n',ww{k(n),2},ww{k(n),1},p(n),repmat('s',p(n)>=2));
 	end
 end
